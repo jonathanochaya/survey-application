5b7a7f70fcfdc263f3c77b81b177895bab6669de
Jonathan Ochaya <wangolojowel@gmail.com>
2022-09-03 21:28:45 +0300

    Added register api endpoint and auth register method that signs up user and returns authorization token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
+    public function register()
+    {
+        $attributes = request()->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', Password::min(6)->mixedCase()->numbers()->symbols()]
+        ]);
 
+        $user = User::create($attributes);
+
+        $token = $user->createToken('user_token')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
 }
