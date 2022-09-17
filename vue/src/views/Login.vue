eed3975ad293ca9a4314dfd5afa7da0f6a7a38c9
Jonathan Ochaya <wangolojowel@gmail.com>
2022-09-17 20:53:46 +0300

    Added login loader for when user is logging in on a slow connection

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -38,11 +38,28 @@
       </div>
 
       <div>
-        <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        <button :disabled="loading" type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
           <span class="absolute inset-y-0 left-0 flex items-center pl-3">
             <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
           </span>
           Sign in
+          <svg v-if="loading"
+            class="animate-spin ml-2 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24">
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
         </button>
       </div>
 
@@ -66,12 +83,18 @@ const user = {
   remember: false
 }
 
+const loading = ref(false);
+
 const login = async () => {
-  try{
+  try {
+    loading.value = true;
+
     await store.dispatch('login', user);
 
+    loading.value = false;
     router.push({ name: 'Dashboard' });
   } catch (err) {
+    loading.value = false;
     errorMsg.value = err.response.data.message;
   }
 }
